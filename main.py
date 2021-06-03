8e871e3ae8557068a1649a501f6c52ebc8681c94
Raymond Chua <ray.r.chua@gmail.com>
2021-06-03 17:24:55 -0400

    set training iteration to 15

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ log = logging.getLogger(__name__)
 coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
 
 args = dotdict({
-    'numIters': 1000,
+    'numIters': 15,
     'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
     'tempThreshold': 15,        #
     'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
